130ccc0757a8926c46a70a767e26a18872a6c052
Leonard develops <120578632+leonard-roepcke@users.noreply.github.com>
2025-06-29 20:40:30 +0000

    Add reset button functionality and update index.html for button display

--- button-script.js
+++ button-script.js
@@ -1,3 +1,9 @@
+ocument.getElementById('reset').addEventListener('click', function() {
+    var money = parseFloat(localStorage.getItem('money')) || 0;
+    money = 0;
+    localStorage.setItem('money', parseFloat(money));
+    document.getElementById('money').textContent = localStorage.getItem('money') + "$";
+});
 document.getElementById('bank').addEventListener('click', function() {
     var money = parseFloat(localStorage.getItem('money')) || 0;
     money += 1;
