8649563e5230e58099c9b2f3384d666e767a96db
Leonard develops <120578632+leonard-roepcke@users.noreply.github.com>
2025-06-29 20:35:14 +0000

    $

--- button-script.js
+++ button-script.js
@@ -2,5 +2,5 @@ document.getElementById('bank').addEventListener('click', function() {
     var money = parseFloat(localStorage.getItem('money')) || 0;
     money += 1;
     localStorage.setItem('money', parseFloat(money));
-    document.getElementById('money').innerText = localStorage.getItem('money');
+    document.getElementById('money').innerText = localStorage.getItem('money') + "$";
 });
