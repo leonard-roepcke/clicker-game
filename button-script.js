b86987bd658defaf5a17b153fae0c73308659ce8
Leonard develops <120578632+leonard-roepcke@users.noreply.github.com>
2025-06-29 20:24:36 +0000

    Fix money display initialization and correct parseFloat typo in button-script.js

--- button-script.js
+++ button-script.js
@@ -1,5 +1,5 @@
 document.getElementById('bank').addEventListener('click', function() {
-    var money = localStorage.getItem('money');
+    var money = parseFloar(localStorage.getItem('money')) || 0;
     localStorage.setItem('money', money + 1);
     document.getElementById('money').innerText = localStorage.getItem('money');
 });
