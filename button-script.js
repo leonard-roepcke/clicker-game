5dbecf6dae1b4bc92c54d35374a4388e1572ad95
core-user <leonard.roepcke.2@gmail.com>
2025-07-06 09:17:20 +0200

    f limit inf

--- button-script.js
+++ button-script.js
@@ -15,6 +15,9 @@ document.getElementById('work').addEventListener('click', function() {
 window.onload = function() {
     let money = parseInt(localStorage.getItem('money')) || 0;
     document.getElementById('money').textContent = money + "$";
+
+    let worker_nr = parseInt(localStorage.getItem('worker')) || 0;
+    document.getElementById('worker_nr').textContent = worker_nr + "/inf"
 };
 document.getElementById('worker').addEventListener('click', function() {
     if (parseFloat(localStorage.getItem('money')) >= 100) {
@@ -24,6 +27,8 @@ document.getElementById('worker').addEventListener('click', function() {
         document.getElementById('money').textContent = money + "$";
         localStorage.setItem('worker', (parseInt(localStorage.getItem('worker')) || 0) + 1);
     }
+    let worker_nr = parseInt(localStorage.getItem('worker')) || 0;
+    document.getElementById('worker_nr').textContent = worker_nr + "/inf"
 });
 
 
@@ -31,4 +36,6 @@ setInterval(function() {
     let money = (parseInt(localStorage.getItem('money')) || 0) + (parseInt(localStorage.getItem('worker')) * 1 || 0);
     localStorage.setItem('money', money);
     document.getElementById('money').textContent = money + "$";
+    let worker_nr = parseInt(localStorage.getItem('worker')) || 0;
+    document.getElementById('worker_nr').textContent = worker_nr + "/inf"
 }, 1000);
