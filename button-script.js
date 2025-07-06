254e857de6e22e7dbc110352a37d8aed2d9b920b
core-user <leonard.roepcke.2@gmail.com>
2025-07-06 09:46:51 +0200

    f hierer

--- button-script.js
+++ button-script.js
@@ -11,6 +11,7 @@ document.getElementById('work').addEventListener('click', function() {
     document.getElementById('money').textContent = money + "$";
 });
 
+
 // Beim Laden der Seite aktuellen Wert anzeigen
 window.onload = function() {
     let money = parseInt(localStorage.getItem('money')) || 0;
@@ -31,11 +32,28 @@ document.getElementById('worker').addEventListener('click', function() {
     document.getElementById('worker_nr').textContent = worker_nr + "/inf"
 });
 
+document.getElementById('hierer').addEventListener('click', function() {
+    if (parseFloat(localStorage.getItem('money')) >= 100) {
+        let money = parseInt(localStorage.getItem('money')) || 0;
+        money -= 10000;
+        localStorage.setItem('money', money);
+        document.getElementById('money').textContent = money + "$";
+        localStorage.setItem('hierer', (parseInt(localStorage.getItem('hierer')) || 0) + 1);
+    }
+});
 
 setInterval(function() {
     let money = (parseInt(localStorage.getItem('money')) || 0) + (parseInt(localStorage.getItem('worker')) * 1 || 0);
     localStorage.setItem('money', money);
-    document.getElementById('money').textContent = money + "$";
     let worker_nr = parseInt(localStorage.getItem('worker')) || 0;
+    let hierer = parseInt(localStorage.getItem('hierer')) || 0;
+    if(money >= 100*hierer){
+        money -= 100*hierer;
+        localStorage.setItem('worker', (parseInt(localStorage.getItem('worker')) || 0) + hierer);
+        localStorage.setItem('money',  money);
+    }
     document.getElementById('worker_nr').textContent = worker_nr + "/inf"
+    document.getElementById('money').textContent = money + "$";
+    
+    
 }, 1000);
